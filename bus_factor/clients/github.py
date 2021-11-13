1abe56ebcefdcebe97f022ad04a650fe8c10715c
MateiCraciun <mateit.craciun@gmail.com>
2021-11-13 18:43:02 +0200

    move contributors processing to executor

--- bus_factor/clients/github.py
+++ bus_factor/clients/github.py
@@ -44,31 +44,19 @@ class GitHubClient:
 
         return result_list
 
-    def get_contributors(self, projects: list[Project]):
+    async def get_contributors(self, contributor_url: Project):
         params = {
             "per_page" : self.CONTRIBUTOR_LIMIT,
             "anon": "true"
         }
 
-        queries = asyncio.gather(*[self._make_call_contributors(
-            project,
-            params
-        ) for project in projects])
-        results = self.event_loop.run_until_complete(queries)
-
-        print(results)
-        print(len(results))
-
-    async def _make_call_contributors(self, project, params):
-        contributors = self._make_call(project.contributor_url, params)
-        project.add_contributors(contributors)
-        return True
+        return await self._make_call(contributor_url, params)
 
     async def _make_call(self, endpoint, params):
         r = requests.get(
             urljoin(self.BASE_URL, endpoint), 
             headers={
-                "Authorization": "access_token {}".format(self.github_key),
+                "Authorization": "token {}".format(self.github_key),
                 "Accept": "application/vnd.github.v3+json"
             },
             params=params
--- bus_factor/executor/executor.py
+++ bus_factor/executor/executor.py
@@ -1,13 +1,15 @@
-from clients.github import GitHubClient
+import asyncio
 
+from clients.github import GitHubClient
 from models.project import Project
 
 class Executor:
     def __init__(self, github_client: GitHubClient):
+        self.event_loop = asyncio.get_event_loop()
         self.github_client = github_client
 
     def execute(self, query_parameters):
-        # retrieve and aggregate github pages
+        # retrieve and aggregate github projects
         projects = [Project(
                 project["name"],
                 project["owner"]["login"],
@@ -18,11 +20,17 @@ class Executor:
                 int(query_parameters["project_count"])
             )
         ]
-        
-        contributors = self.github_client.get_contributors(
-            projects
-        )
-        # filter pages by criteria
+
+        contributor_queries = asyncio.gather(*[self.get_contributors(
+            project
+        ) for project in projects])
+        results = self.event_loop.run_until_complete(contributor_queries)
+        # filter projects by criteria
 
         # print out results
         pass
+
+    async def get_contributors(self, project: Project):
+        contributors = await self.github_client.get_contributors(project.contributor_url)
+        project.add_contributors(contributors)
+        return project
